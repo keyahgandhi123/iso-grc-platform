5ffaf6a3175904eb78f094022631fa8e5285f6d8
Keyah Gandhi <keyahgandhi@Keyahs-MacBook-Air.local>
2026-03-11 10:06:27 +0530

    Fix Render server startup

--- app.py
+++ app.py
@@ -798,10 +798,11 @@ def report_history():
 # RUN
 # ======================
 
+import os
+
 if __name__ == "__main__":
 
     with app.app_context():
-
         db.create_all()
 
         admin = User.query.filter_by(username="admin").first()
@@ -816,4 +817,5 @@ if __name__ == "__main__":
             db.session.add(admin)
             db.session.commit()
 
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
